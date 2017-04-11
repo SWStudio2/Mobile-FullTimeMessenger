2f21fc65d15ed0411d04561ce960d2ca54f75269
Kewalin Sakawattananon <cisdkews@th.ibm.com>
2017-04-11 22:22:28 +0700

    Initial Commit

---
+++ FoodDelivery-FullTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodDelivery-FullTime
+//
+//  Created by Kewalin Sakawattananon on 4/11/2560 BE.
+//  Copyright © 2560 BSD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
