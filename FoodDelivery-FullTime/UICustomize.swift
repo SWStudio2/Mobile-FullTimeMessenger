4f22ba2aeaa2aae326677135bf9ca13f54880be3
Kewalin Sakawattananon <cisdkews@th.ibm.com>
2017-04-14 14:02:55 +0700

    login

---
+++ FoodDelivery-FullTime/UICustomize.swift
@@ -0,0 +1,9 @@
+//
+//  UICustomize.swift
+//  FoodDelivery-FullTime
+//
+//  Created by Kewalin Sakawattananon on 4/14/2560 BE.
+//  Copyright © 2560 BSD. All rights reserved.
+//
+
+import Foundation
