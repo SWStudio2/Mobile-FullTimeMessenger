989aa09ccda94b31cdc94a9d995f4b64e74b3665
Kewalin Sakawattananon <cisdkews@th.ibm.com>
2017-04-24 00:23:01 +0700

    Bike Station

---
+++ FoodDelivery-FullTime/StationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StationViewController.swift
+//  FoodDelivery-FullTime
+//
+//  Created by Kewalin Sakawattananon on 4/23/2560 BE.
+//  Copyright © 2560 BSD. All rights reserved.
+//
+
+import UIKit
+
+class StationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
